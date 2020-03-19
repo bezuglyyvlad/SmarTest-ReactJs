ed44ff5e8f9467f46ffe80518f42ac574b6038f5
Vlad Bezuhlyi <bezuglyyvlad@gmail.com>
2020-03-19 18:51:25 +0200

    Test with 2 type question

--- src/components/Profile/DeleteAccount/DeleteAccount.jsx
+++ src/components/Profile/DeleteAccount/DeleteAccount.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "@material-ui/core/Link";
 import {DialogCreator} from "../../common/UIElements";
+import Button from "@material-ui/core/Button";
 
 const DeleteAccount = React.memo(({deleteUser, userId}) => {
     const [open, setOpen] = React.useState(false);
@@ -24,7 +25,10 @@ const DeleteAccount = React.memo(({deleteUser, userId}) => {
                 Удалить аккаунт
             </Link>
             <DialogCreator open={open} handleClose={handleClose} title='Удаление аккаунта'
-                           text='Вы действительно хотите удалить аккаунт?' actionConfirm={deleteAccount}/>
+                           text='Вы действительно хотите удалить аккаунт?' confirmButton={
+                <Button onClick={deleteAccount} color="primary">
+                    Да
+                </Button>}/>
         </>
     )
 })
