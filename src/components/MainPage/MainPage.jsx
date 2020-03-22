16b6769e540b6bd426b957f67d3b9858eebb6c86
Vlad Bezuhlyi <bezuglyyvlad@gmail.com>
2020-03-22 15:02:47 +0200

    Logo for pwa and main page

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,21 +1,62 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import {NavLink} from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
     root: {
-        fontSize: '50px',
-        textAlign: 'center'
+        textAlign: 'center',
+        marginTop: theme.spacing(5),
     },
+    paper: {
+        marginBottom: theme.spacing(2),
+        padding: theme.spacing(15, 2)
+    },
+    title: {
+        marginBottom: theme.spacing(2)
+    },
+    button: {
+        marginTop: theme.spacing(5)
+    }
 }));
 
-const MainPage = React.memo(({signIn, isAuth}) => {
-
+const MainPage = React.memo(() => {
     const classes = useStyles();
-
     return (
-        <Container component="main" maxWidth="xs" className={classes.root}>
-            Main Page
+        <Container component="main" maxWidth="lg" className={classes.root}>
+            <Paper className={classes.paper}>
+                <Typography variant='h4' component='h1' className={classes.title}>
+                    Попробуйте динамическое тестирование с SmarTest
+                </Typography>
+                <Typography variant='h5'>
+                    Тест будет подстраиваться под ваш уровень знаний.
+                    Проверь свои знания прямо сейчас.
+                </Typography>
+                <Button component={NavLink} to='/category' variant="contained" color="primary"
+                        className={classes.button}>
+                    Начать
+                </Button>
+            </Paper>
+            <Paper className={classes.paper}>
+                <Typography variant='h4' component='h1' className={classes.title}>
+                    Наш сервис сделан в виде PWA приложения
+                </Typography>
+                <Typography variant='h5'>
+                    Теперь не нужно скачивать приложение через сторонние магазины. Нужно всего лишь подтвердить
+                    предложение для установки либо сделать это вручную (смотреть в руководстве для вашего браузера)
+                </Typography>
+            </Paper>
+            <Paper className={classes.paper}>
+                <Typography variant='h4' component='h1' className={classes.title}>
+                    Стать Експертом
+                </Typography>
+                <Typography variant='h5'>
+                    Хотите стать Экспертом и создавать тесты самому? Тогда свяжитесь с администратором - notrealemail@gmail.com
+                </Typography>
+            </Paper>
         </Container>
     );
 });
